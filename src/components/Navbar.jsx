4ca9029451f15b1dbb77c684a8c8f1e1c9164e01
Dhruv6397 <vermadhruv6397@gmail.com>
2024-01-17 01:23:42 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavLogo from './NavLogo.png'
 import './Nav.css';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import NotificationsNoneRoundedIcon from '@mui/icons-material/NotificationsNoneRounded';
 
 export default function Navbar() {
   return (
@@ -8,13 +10,18 @@ export default function Navbar() {
     <nav> 
         <header className='logo'><img className='imglogo' src={NavLogo} alt='NavLogo'/></header>
         <ul className='list'>    
-            <li className='item'  >Home</li>
-            <li className='item'  >TV Shows</li>
-            <li className='item'  >Movies</li>
-            <li className='item'  >New & Popular</li>
-            <li className='item'  >My List</li>
-            <li className='item'  >Browse by Languages</li>
+            <li className='item'>Home</li>
+            <li className='item'>TV Shows</li>
+            <li className='item'>Movies</li>
+            <li className='item'>New & Popular</li>
+            <li className='item'>My List</li>
+            <li className='item'>Browse by Languages</li>
         </ul>
+        <div className="sidcmp">
+            <SearchRoundedIcon/>
+            <span>chlidren</span>
+            <NotificationsNoneRoundedIcon/>
+        </div>
       </nav>
     </>
   )
